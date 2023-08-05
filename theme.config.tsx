5681a932eca2e9b5b79f2d208596d9da743f6427
iamleomoko <mokoliyolo@gmail.com>
2023-08-05 13:19:45 +0200

    Site cleanup

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,26 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span><b>Leo Moko Doc</b></span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/iamleomoko/doc-leomoko',
   },
-  chat: {
-    link: 'https://discord.com',
+  docsRepositoryBase: 'https://github.com/iamleomoko/doc-leomoko/tree/master',
+  useNextSeoProps() {
+     return {
+      titleTemplate: '%s'
+    }
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+   footer: {
+    text:(
+      <div className="flex w-full flex-col items-center sm:items-start">
+          <p>
+            © {new Date().getFullYear()} All rights reserved <a href="https://www.leomoko.com/"> by Leo Moko</a>.
+          </p>
+      </div> 
+    ),
+  }
 }
+
 
 export default config
